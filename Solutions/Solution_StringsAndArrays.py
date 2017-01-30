1da2e0dcc4b1a8d2d4731b56cb3628dab028067e
mpmart20 <mp.mart20@gmail.com>
2017-01-30 15:42:41 -0800

    testing reconfig

--- Solutions/Solution_StringsAndArrays.py
+++ Solutions/Solution_StringsAndArrays.py
@@ -124,7 +124,6 @@ def setToZero(matrix):
             matrix[curr][x] = 0
     return matrix
 
-
 def isSubstring():
     """
     assume you have a method isSubstring which checks if one word is a substring of another. Given two strings, s1 and s2, write the code to check if s2 is a rotation of s1 using only one call to isSubstring
